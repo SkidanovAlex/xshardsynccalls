b43efb36150a024cd0d530ba64f538617da56419
Alex Skidanov <alex@nearprotocol.com>
2019-02-16 22:19:19 -0800

    Something appears to be working for a case of two shards with a deadlock

--- primitives.py
+++ primitives.py
@@ -6,6 +6,10 @@ def winning_tx(tx1, tx2):
     return tx1 if tx1.tx_id < tx2.tx_id else tx2
 
 
+def losing_tx(tx1, tx2):
+    return tx1 if tx1.tx_id > tx2.tx_id else tx2
+
+
 class Shard(object):
     def __init__(self, shard_id, parent_id, child_ids):
         super(Shard, self).__init__()
@@ -14,32 +18,84 @@ class Shard(object):
         self.child_ids = child_ids
         self.blocks = []
 
-    def process_message(self, msg, outbox, pending_txs, locks):
+    def process_message(self, msg, outbox, pending_txs, locks, graph):
         if msg.msg_type == Message.EXECUTE:
-            self.process_tx(msg.tx, outbox, pending_txs, locks)
+            self.process_tx(msg.tx, outbox, pending_txs, locks, graph)
         elif msg.msg_type == Message.RETURN:
             self.process_return(msg.tx, outbox, pending_txs, locks)
         elif msg.msg_type == Message.BLOCKED:
-            self.process_blocked(msg.tx, msg.tx_on, outbox)
-        elif msg.msg_type == Message.ROLLBACK:
-            # TODO
+            self.process_blocked(msg.tx, msg.tx_on, None, outbox, pending_txs, locks, graph)
+        elif msg.msg_type == Message.ROLLBACK_FORWARD:
+            self.process_rollback_forward(msg.tx, outbox, pending_txs, locks)
+        elif msg.msg_type == Message.ROLLBACK_BACKWARD:
+            self.process_rollback_backward(msg.tx, outbox, pending_txs, locks)
             pass
         else:
             assert False, "Unknown message type %s" % msg.msg_type
 
-    def process_blocked(self, tx, tx_on, outbox):
-        outgoing_msg = Message_Blocked(tx, tx_on)
-
-        if outgoing_msg.target_shard_id == self.shard_id:
-            # TODO
-            pass
-
+    def process_rollback_forward(self, tx, outbox, pending_txs, locks):
+        assert tx.steps[tx.step_id].shard_id == self.shard_id
+        for i, pending_tx in enumerate(pending_txs):
+            if pending_tx.tx_id == tx.tx_id:
+                pending_txs = pending_txs[:i] + pending_txs[i+1:]
+                outgoing_msg = Message_Rollback_Backward(Transaction.clone_tx(tx, tx.step_id - 1))
+                if outgoing_msg.target_shard_id not in outbox:
+                    outbox[outgoing_msg.target_shard_id] = []
+                outbox[outgoing_msg.target_shard_id].append(outgoing_msg)
+                break
         else:
+            # no pending tx -- send further
+            assert tx.step_id + 1 < len(tx.steps), "A rollback forward overflow" # in theory can happen, but scenarios are designed not to have it happen
+            outgoing_msg = Message_Rollback_Forward(Transaction.clone_tx(tx, tx.step_id + 1))
             if outgoing_msg.target_shard_id not in outbox:
                 outbox[outgoing_msg.target_shard_id] = []
             outbox[outgoing_msg.target_shard_id].append(outgoing_msg)
 
-    def process_tx(self, tx, outbox, pending_txs, locks):
+    def process_rollback_backward(self, tx, outbox, pending_txs, locks):
+        assert tx.steps[tx.step_id].shard_id == self.shard_id
+        contract_id = tx.steps[tx.step_id].contract_id
+        if contract_id not in locks or locks[contract_id].tx_id != tx.tx_id:
+            # the tx is already rolled back
+            return
+        del locks[contract_id]
+
+        if tx.step_id > 0:
+            outgoing_msg = Message_Rollback_Backward(Transaction.clone_tx(tx, tx.step_id - 1))
+            if outgoing_msg.target_shard_id not in outbox:
+                outbox[outgoing_msg.target_shard_id] = []
+            outbox[outgoing_msg.target_shard_id].append(outgoing_msg)
+        else:
+            if VERBOSE: print("Restarting %s" % tx.tx_id)
+            pending_txs.append(tx) # restart the transaction
+
+    def process_blocked(self, tx, tx_on, who_cares, outbox, pending_txs, locks, graph):
+        if (tx.tx_id, tx_on.tx_id) in graph:
+            return
+
+        if who_cares is None:
+            who_cares = losing_tx(tx, tx_on)
+
+        graph[(tx.tx_id, tx_on.tx_id)] = (tx, tx_on, who_cares)
+
+        if who_cares.originating_shard_id != self.shard_id:
+            outgoing_msg = Message_Blocked(tx, tx_on, who_cares.originating_shard_id)
+            if outgoing_msg.target_shard_id not in outbox:
+                outbox[outgoing_msg.target_shard_id] = []
+            outbox[outgoing_msg.target_shard_id].append(outgoing_msg)
+
+        else:
+            if (tx_on.tx_id, tx.tx_id) in graph: # deadlock
+                if VERBOSE: print("Deadlock detected %s->%s, owner: %s, on shard %s" % (tx.tx_id, tx_on.tx_id, who_cares.tx_id, self.shard_id))
+                self.process_rollback_forward(Transaction.clone_tx(who_cares, 0), outbox, pending_txs, locks)
+            
+        for other_tx, other_tx_on, other_who_cares in [v for v in graph.values()]:
+            if other_tx_on.tx_id == tx.tx_id:
+                self.process_blocked(other_tx, tx_on, losing_tx(who_cares, other_who_cares), outbox, pending_txs, locks, graph)
+            if other_tx.tx_id == tx_on.tx_id:
+                self.process_blocked(tx, other_tx_on, losing_tx(who_cares, other_who_cares), outbox, pending_txs, locks, graph)
+
+
+    def process_tx(self, tx, outbox, pending_txs, locks, graph):
         assert tx.steps[tx.step_id].shard_id == self.shard_id
         contract_id = tx.steps[tx.step_id].contract_id
         
@@ -50,7 +106,7 @@ class Shard(object):
 
             if VERBOSE: print("%s(%s, %s) blocked on %s(%s, %s)" % (tx.tx_id, self.shard_id, tx.steps[tx.step_id].contract_id, tx_on.tx_id, tx_on.steps[tx_on.step_id].shard_id, tx_on.steps[tx_on.step_id].contract_id))
 
-            self.process_blocked(tx, tx_on, outbox)
+            self.process_blocked(tx, tx_on, None, outbox, pending_txs, locks, graph)
             return
 
         if tx.step_id + 1 == len(tx.steps):
@@ -71,11 +127,11 @@ class Shard(object):
         assert tx.steps[tx.step_id].shard_id == self.shard_id
         contract_id = tx.steps[tx.step_id].contract_id
         
+        del locks[contract_id]
+
         if tx.step_id == 0:
             pass # transaction is completed
         else:
-            del locks[contract_id]
-
             outgoing_msg = Message_Return(Transaction.clone_tx(tx, tx.step_id - 1))
             if outgoing_msg.target_shard_id not in outbox:
                 outbox[outgoing_msg.target_shard_id] = []
@@ -83,19 +139,21 @@ class Shard(object):
 
 
 class Block(object):
-    def __init__(self, prev_block, outbox, locks, pending_txs):
+    def __init__(self, prev_block, outbox, locks, pending_txs, graph):
         super(Block, self).__init__()
         self.prev_block = prev_block
         self.outbox = outbox
         self.locks = locks
         self.pending_txs = pending_txs
+        self.graph = graph
 
 
 class Message(object):
     EXECUTE = 1
     RETURN = 2
     BLOCKED = 3
-    ROLLBACK = 4
+    ROLLBACK_FORWARD = 4
+    ROLLBACK_BACKWARD = 5
 
     def __init__(self, msg_type, target_shard_id):
         super(Message, self).__init__()
@@ -116,15 +174,21 @@ class Message_Return(Message):
 
 
 class Message_Blocked(Message):
-    def __init__(self, tx, tx_on):
-        super(Message_Blocked, self).__init__(Message.BLOCKED, winning_tx(tx, tx_on))
+    def __init__(self, tx, tx_on, target_shard_id):
+        super(Message_Blocked, self).__init__(Message.BLOCKED, target_shard_id)
         self.tx = tx
         self.tx_on = tx_on
 
 
-class Message_Rollback(Message):
+class Message_Rollback_Forward(Message):
     def __init__(self, tx):
-        super(Message_Rollback, self).__init__(Message.ROLLBACK, tx.originating_shard_id)
+        super(Message_Rollback_Forward, self).__init__(Message.ROLLBACK_FORWARD, tx.steps[tx.step_id].shard_id)
+        self.tx = tx
+
+
+class Message_Rollback_Backward(Message):
+    def __init__(self, tx):
+        super(Message_Rollback_Backward, self).__init__(Message.ROLLBACK_BACKWARD, tx.steps[tx.step_id].shard_id)
         self.tx = tx
 
 
--- simulation.py
+++ simulation.py
@@ -14,7 +14,8 @@ class TxState(object):
 
     def reset(self):
         self.latest_step = 0
-        self.latest_rollback = 0
+        self.latest_rollback_forward = 0
+        self.latest_rollback_backward = len(self.tx.steps)
         self.latest_return = len(self.tx.steps)
         
 
@@ -33,14 +34,14 @@ class Simulation(object):
             parent_id = parents_map[shard_id] if shard_id in parents_map else None
             shard = Shard(shard_id, parent_id, children)
             self.shards.append(shard)
-            shard.blocks.append(Block(None, {}, {}, []))
+            shard.blocks.append(Block(None, {}, {}, [], {}))
 
         for i, steps in enumerate(transactions):
             tx = Transaction(i, steps[0][0], [Step(x[0], x[1]) for x in steps])
             self.txstates.append(TxState(tx))
 
             shard = self.shards[tx.originating_shard_id]
-            shard.process_tx(tx, shard.blocks[-1].outbox, shard.blocks[-1].pending_txs, shard.blocks[-1].locks)
+            shard.process_tx(tx, shard.blocks[-1].outbox, shard.blocks[-1].pending_txs, shard.blocks[-1].locks, shard.blocks[-1].graph)
 
 
     def is_finished(self):
@@ -52,30 +53,34 @@ class Simulation(object):
 
 
     def step(self):
+        if VERBOSE: print("EXECUTING A STEP")
         new_blocks = {}
         for shard in self.shards:
             outbox = {}
             pending_txs = []
             locks = {k: v for (k, v) in shard.blocks[-1].locks.items()}
+            graph = {}
 
             for tx in shard.blocks[-1].pending_txs:
-                shard.process_tx(tx, outbox, pending_txs, locks)
+                shard.process_tx(tx, outbox, pending_txs, locks, graph)
 
             for neighbor_id in [shard.parent_id] + shard.child_ids:
                 if neighbor_id is None or shard.shard_id not in self.shards[neighbor_id].blocks[-1].outbox:
                     continue
                 for msg in self.shards[neighbor_id].blocks[-1].outbox[shard.shard_id]:
-                    shard.process_message(msg, outbox, pending_txs, locks)
+                    shard.process_message(msg, outbox, pending_txs, locks, graph)
                     if msg.msg_type == Message.EXECUTE:
                         self.txstates[msg.tx.tx_id].latest_step = max(self.txstates[msg.tx.tx_id].latest_step, msg.tx.step_id)
-                    if msg.msg_type == Message.ROLLBACK:
-                        self.txstates[msg.tx.tx_id].latest_rollback = max(self.txstates[msg.tx.tx_id].latest_rollback, msg.tx.step_id)
-                        if self.txstates[msg.tx.tx_id].latest_rollback == self.txstates[msg.tx.tx_id].latest_step:
+                    if msg.msg_type == Message.ROLLBACK_FORWARD:
+                        self.txstates[msg.tx.tx_id].latest_rollback_forward = max(self.txstates[msg.tx.tx_id].latest_rollback_forward, msg.tx.step_id)
+                    if msg.msg_type == Message.ROLLBACK_BACKWARD:
+                        self.txstates[msg.tx.tx_id].latest_rollback_backward = min(self.txstates[msg.tx.tx_id].latest_rollback_backward, msg.tx.step_id)
+                        if self.txstates[msg.tx.tx_id].latest_rollback_backward == 0:
                             self.txstates[msg.tx.tx_id].reset()
                     if msg.msg_type == Message.RETURN:
                         self.txstates[msg.tx.tx_id].latest_return = min(self.txstates[msg.tx.tx_id].latest_return, msg.tx.step_id)
 
-            new_blocks[shard.shard_id] = Block(shard.blocks[-1], outbox, locks, pending_txs)
+            new_blocks[shard.shard_id] = Block(shard.blocks[-1], outbox, locks, pending_txs, graph)
 
         for shard in self.shards:
             shard.blocks.append(new_blocks[shard.shard_id])
@@ -92,6 +97,6 @@ if __name__ == "__main__":
 
     else:
         for txstate in simulation.txstates:
-            print("TX %s: step %s, rollback %s, return %s" % (txstate.tx.tx_id, txstate.latest_step, txstate.latest_rollback, txstate.latest_return))
+            print("TX %s: step %s, rollbacks %s %s, return %s" % (txstate.tx.tx_id, txstate.latest_step, txstate.latest_rollback_forward, txstate.latest_rollback_backward, txstate.latest_return))
         assert False, "Haven't finished in %s steps" % MAX_STEPS
 
